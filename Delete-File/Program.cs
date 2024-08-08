eccf32d440ce4cb64b3f0b08b44bd299f256b45b
Yaduveesh Pratap Singh <yaduveeshp.singh@grovvey.com>
2024-08-08 09:22:06 +0530

    Add project files.

---
+++ Delete-File/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class DeleteFile
+{
+    public static void Main()
+    {
+        string fileName = @"testfile.txt";
+
+        Console.Write("\n\n Remove a file from the disk (at first create the file ) :\n");
+        Console.Write("--------------------------------------------------------------\n");
+
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+            Console.WriteLine(" The file {0} deleted successfully.\n\n", fileName);
+        }
+        else
+        {
+            Console.WriteLine(" File does not exist");
+            Console.ReadKey();
+        }
+    }
+}
